b84ef03a5d7ac6bda4ca914a0c195bd47aec7de1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-27 12:58:39 -0800

    Completed Part 3.

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -39,11 +39,30 @@ $("#weather-form").on('submit', showWeather);
 
 // PART 3: ORDER MELONS
 
+function showMelonOrderSuccess(result) {
+    console.log(result);
+    if (result.result_code == "ERROR") {
+        $("#order-status").addClass("order-error");
+    }
+    alert(result.msg); 
+}
+
+
 function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+    var formInputs = {
+        "melon_type": $("#melon-type-field").val(),
+        "qty": $("#qty-field").val()
+    };
+
+    $.post("/order-melons.json",
+           formInputs,
+           showMelonOrderSuccess
+           );
 }
 
 $("#order-form").on('submit', orderMelons);
